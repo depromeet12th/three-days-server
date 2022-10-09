f435240c0c1938f244e953ce1a57c29088022c8f
김주현 (Juhyun Kim)/Commerce <juhyun.kim_rnd@wesang.com>
2022-10-09 16:59:37 +0900

    dep-18 feat : add toString()

--- app/src/main/java/com/depromeet/threedays/front/domain/model/Member.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/model/Member.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@ToString
 public class Member {
 	private Long memberId;
 	private String nickname;
