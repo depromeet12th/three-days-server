cbc3585e2708ed2f30bb68b1334ef85de1cfead2
김주현 (Juhyun Kim)/Commerce <juhyun.kim_rnd@wesang.com>
2022-10-09 16:52:10 +0900

    dep-18 feat : delete unnecessary repository

--- app/src/main/java/com/depromeet/threedays/front/repository/MemberRepository.java
+++ app/src/main/java/com/depromeet/threedays/front/repository/MemberRepository.java
@@ -3,9 +3,7 @@ package com.depromeet.threedays.front.repository;
 import com.depromeet.threedays.data.entity.MemberEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
 	Optional<MemberEntity> findById(final Long memberId);
