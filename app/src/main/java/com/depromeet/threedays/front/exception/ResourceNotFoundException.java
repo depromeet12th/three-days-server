07691e4388f34adffbcd6b0003ce302d23b26dcc
김주현 (Juhyun Kim)/Commerce <juhyun.kim_rnd@wesang.com>
2022-10-09 16:57:09 +0900

    dep-18 feat : delete unnecessary getter

--- app/src/main/java/com/depromeet/threedays/front/exception/ResourceNotFoundException.java
+++ app/src/main/java/com/depromeet/threedays/front/exception/ResourceNotFoundException.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @NoArgsConstructor
 public class ResourceNotFoundException extends RuntimeException {
 
