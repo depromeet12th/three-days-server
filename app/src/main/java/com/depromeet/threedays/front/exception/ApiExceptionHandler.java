2d93096d10a66b2faa61a516e7970f0c16bcb5a9
saint6839 <saint6839@gmail.com>
2022-10-20 18:14:57 +0900

    DEP-60 feat : add ResourceNotFoundException handle test method

--- app/src/main/java/com/depromeet/threedays/front/exception/ApiExceptionHandler.java
+++ app/src/main/java/com/depromeet/threedays/front/exception/ApiExceptionHandler.java
@@ -1,8 +1,15 @@
 package com.depromeet.threedays.front.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.depromeet.threedays.front.controller")
 public class ApiExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException) {
+        return new ResponseEntity<>(resourceNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
