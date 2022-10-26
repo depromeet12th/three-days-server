da6a3ec244d7eb475319766e9b7344309b348faa
gojung <wjdrndk96@gmail.com>
2022-10-26 10:04:24 +0900

    DEP-62 usecase 코드를 OAuthManager로 분리

--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/oauth/SaveOAuthUseCase.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/oauth/SaveOAuthUseCase.java
@@ -1,14 +1,11 @@
 package com.depromeet.threedays.front.domain.usecase.oauth;
 
 import com.depromeet.threedays.data.entity.member.MemberEntity;
-import com.depromeet.threedays.front.client.OAuthClient;
 import com.depromeet.threedays.front.client.OAuthManager;
 import com.depromeet.threedays.front.client.model.OAuthInfo;
-import com.depromeet.threedays.front.client.property.OAuthProperty;
 import com.depromeet.threedays.front.controller.command.oauth.OAuthCommand;
 import com.depromeet.threedays.front.domain.converter.member.MemberConverter;
 import com.depromeet.threedays.front.domain.model.Member;
-import com.depromeet.threedays.front.domain.model.Token;
 import com.depromeet.threedays.front.repository.MemberRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
@@ -24,11 +21,7 @@ public class SaveOAuthUseCase {
 	private final MemberRepository memberRepository;
 
 	public Member execute(OAuthCommand command) throws JsonProcessingException {
-		OAuthClient client = oAuthManager.getOAuthClient(command.getCertificationSubject());
-		OAuthProperty property = oAuthManager.getOAuthProperty(command.getCertificationSubject());
-		OAuthInfo info =
-				client.readOAuthUserData(
-						property, new Token(command.getAccessToken(), command.getIdToken()));
+		OAuthInfo info = oAuthManager.getOAuthInfo(command);
 
 		return MemberConverter.from(
 				memberRepository.findByName(info.getName()).orElse(join(info, command)));
