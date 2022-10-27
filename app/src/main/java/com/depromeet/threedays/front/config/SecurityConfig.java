8cc713c4e368108d807e0b22b680e03b53694a54
saint6839 <saint6839@gmail.com>
2022-10-27 13:02:07 +0900

    DEP-61 refactor : AbstractPreAuthenticatedProcessingFilter를 JwtAuthenticationFilter가 대체하도록 수정

--- app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
+++ app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -27,7 +28,7 @@ public class SecurityConfig {
 				.antMatchers("/api/v1/**")
 				.permitAll();
 
-		http.addFilter(jwtAuthenticationFilter());
+		http.addFilterAt(jwtAuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class);
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		return http.build();
