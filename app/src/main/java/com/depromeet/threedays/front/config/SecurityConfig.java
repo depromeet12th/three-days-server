643fdaf77d1cc8b98da57a33e055fd868a92aaed
saint6839 <saint6839@gmail.com>
2022-10-27 09:55:12 +0900

    DEP-61 feat : Spring Security Config 구현
    -swagger 경로 접근 허용
    -api/v1/** 에 대한 접근 허용
    -JwtAuthenticationFilter 생성 및 추가

---
+++ app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.depromeet.threedays.front.config;
+
+import com.depromeet.threedays.front.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.formLogin().disable();
+		http.httpBasic().disable();
+
+		http.authorizeRequests()
+				.antMatchers("/swagger-ui/index.html#/")
+				.permitAll()
+				.antMatchers("/api/v1/**")
+				.permitAll();
+
+		http.addFilter(jwtAuthenticationFilter());
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		return http.build();
+	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider);
+		jwtAuthenticationFilter.setAuthenticationManager(new ProviderManager(jwtTokenProvider));
+		return jwtAuthenticationFilter;
+	}
+}
