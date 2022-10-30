4e4a9d3006496d9ada50d46050a62ff3683fcbf7
saint6839 <saint6839@gmail.com>
2022-10-30 10:45:39 +0900

    DEP-61 feat : profile별 권한 메서드 분리

--- app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
+++ app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.depromeet.threedays.front.config;
 
-import com.depromeet.threedays.front.controller.member.TokenAuthenticationProvider;
-import com.depromeet.threedays.front.controller.member.TokenGenerator;
-import com.depromeet.threedays.front.filter.TokenAuthenticationFilter;
+import com.depromeet.threedays.front.filter.token.TokenGenerator;
+import com.depromeet.threedays.front.filter.token.TokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,10 +18,11 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
 public class SecurityConfig {
 
 	private final TokenGenerator tokenGenerator;
-	private final TokenAuthenticationProvider tokenAuthenticationProvider;
+	private final AuthenticationProvider tokenAuthenticationProvider;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Profile(value = "local")
+	public SecurityFilterChain localSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.formLogin().disable();
 		http.httpBasic().disable();
@@ -31,14 +33,52 @@ public class SecurityConfig {
 				.antMatchers("/api/v1/**")
 				.permitAll();
 
-		http.addFilterAt(jwtAuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class);
+		http.addFilterAt(tokenAuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class);
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		return http.build();
 	}
 
 	@Bean
-	public TokenAuthenticationFilter jwtAuthenticationFilter() {
+	@Profile(value = "dev")
+	public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.formLogin().disable();
+		http.httpBasic().disable();
+
+		http.authorizeRequests()
+				.antMatchers("/swagger-ui/index.html#/")
+				.permitAll()
+				.antMatchers("/api/v1/**")
+				.permitAll();
+
+		http.addFilterAt(tokenAuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		return http.build();
+	}
+
+	@Bean
+	@Profile(value = "prod")
+	public SecurityFilterChain prodSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.formLogin().disable();
+		http.httpBasic().disable();
+
+		http.authorizeRequests()
+			.antMatchers("/swagger-ui/index.html#/")
+			.permitAll()
+			.antMatchers("/api/v1/**")
+			.permitAll();
+
+		http.addFilterAt(tokenAuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		return http.build();
+	}
+
+	@Bean
+	public TokenAuthenticationFilter tokenAuthenticationFilter() {
 		TokenAuthenticationFilter tokenAuthenticationFilter =
 				new TokenAuthenticationFilter(tokenGenerator);
 		tokenAuthenticationFilter.setAuthenticationManager(
