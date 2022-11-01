4385d3f78eab138fbf6ef99b887b4ad715b15e0a
gojung <wjdrndk96@gmail.com>
2022-11-01 22:16:32 +0900

    DEP-62 remove unnecessary property values

--- app/src/main/java/com/depromeet/threedays/front/client/property/GoogleOAuthProperty.java
+++ app/src/main/java/com/depromeet/threedays/front/client/property/GoogleOAuthProperty.java
@@ -6,11 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class GoogleOAuthProperty extends OAuthProperty {
 	public GoogleOAuthProperty(
-			@Value("${google.web.client.id}") String clientId,
-			@Value("${google.web.client.secret}") String clientSecret,
-			@Value("${google.web.redirect.uri}") String redirectUri,
-			@Value("${google.access.token.uri}") String accessTokenUri,
 			@Value("${google.user.uri}") String userUri) {
-		super(redirectUri, clientId, clientSecret, accessTokenUri, userUri);
+		super(userUri);
 	}
 }
--- app/src/main/java/com/depromeet/threedays/front/client/property/OAuthProperty.java
+++ app/src/main/java/com/depromeet/threedays/front/client/property/OAuthProperty.java
@@ -5,25 +5,10 @@ import lombok.Getter;
 @Getter
 public abstract class OAuthProperty {
 
-	protected String redirectUri;
-
-	protected String clientId;
-
-	protected String clientSecret;
-	protected String accessTokenUri;
-
 	protected String userUri;
 
 	protected OAuthProperty(
-			String redirectUri,
-			String clientId,
-			String clientSecret,
-			String accessTokenUri,
 			String userUri) {
-		this.redirectUri = redirectUri;
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.accessTokenUri = accessTokenUri;
 		this.userUri = userUri;
 	}
 }
