0b7cd92ea4e7eb4ce9ff9ad01a519aa2ac3680b4
saint6839 <saint6839@gmail.com>
2022-11-02 15:57:18 +0900

    DEP-61 refactor : authProvider가 직접적인 구현체에 의존하도록 변경

--- app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
+++ app/src/main/java/com/depromeet/threedays/front/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.depromeet.threedays.front.config;
 
+import com.depromeet.threedays.front.filter.token.AuthProvider;
 import com.depromeet.threedays.front.filter.token.TokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final AuthenticationProvider tokenAuthenticationProvider;
+	private final AuthProvider authProvider;
 
 	@Bean
 	@Profile(value = "local")
@@ -79,7 +79,7 @@ public class SecurityConfig {
 	public TokenAuthenticationFilter tokenAuthenticationFilter() {
 		TokenAuthenticationFilter tokenAuthenticationFilter = new TokenAuthenticationFilter();
 		tokenAuthenticationFilter.setAuthenticationManager(
-				new ProviderManager(tokenAuthenticationProvider));
+				new ProviderManager(authProvider));
 		return tokenAuthenticationFilter;
 	}
 }
