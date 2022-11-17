6b42c0a639139c5d1567844083ba50c14cb05fcb
saint6839 <saint6839@gmail.com>
2022-11-17 14:27:27 +0900

    DEP-124 feat : 첫 습관 달성, 예정 습관 달성일 이후에 달성 요청을 보냈을 경우에 대한 HabitAchievementEntity 변환 메서드 구현

--- app/src/main/java/com/depromeet/threedays/front/domain/converter/habit/HabitAchievementConverter.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/converter/habit/HabitAchievementConverter.java
@@ -1,6 +1,8 @@
 package com.depromeet.threedays.front.domain.converter.habit;
 
 import com.depromeet.threedays.data.entity.habit.HabitAchievementEntity;
+import com.depromeet.threedays.data.entity.habit.HabitEntity;
+import com.depromeet.threedays.front.controller.request.habit.SaveHabitAchievementRequest;
 import com.depromeet.threedays.front.domain.model.habit.HabitAchievement;
 import lombok.experimental.UtilityClass;
 
@@ -14,5 +16,16 @@ public class HabitAchievementConverter {
 				.habitAchievementId(entity.getId())
 				.sequence(entity.getSequence())
 				.build();
+	}
+
+	public static HabitAchievementEntity to(HabitEntity habitEntity, SaveHabitAchievementRequest request, int dayDifference) {
+		return HabitAchievementEntity.builder()
+									 .habitId(habitEntity.getId())
+									 .memberId(habitEntity.getMemberId())
+									 .achievementDate(request.getAchievementDate())
+									 .nextAchievementDate(request.getAchievementDate()
+																 .plusDays(dayDifference))
+									 .sequence(1)
+									 .build();
 	}
 }
