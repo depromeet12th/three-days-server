31aeb0be2c5038dcf6c28d9c81dfa78c4f46fa5d
saint6839 <saint6839@gmail.com>
2022-11-17 21:50:40 +0900

    DEP-138 feat : 습관 달성 취소 기능 구현
    - 지우려고 하는 achievement의 날짜와 오늘 날짜가 같다면 삭제
    - 지우려고 하는 achievement의 sequence가 3의 배수였다면, 이 당시 achivement와 함께 얻었던 RewardHistory도 삭제
      - 응답 Habit에 총 리워드 수를 1감소 시켜서 반환
    - 지우려고 하는 achievement의 sequence가 3의 배수가 아니라면, 해당 achievement만 삭제

--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitAchievementUseCase.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitAchievementUseCase.java
@@ -1,6 +1,12 @@
 package com.depromeet.threedays.front.domain.usecase.habit;
 
+import com.depromeet.threedays.data.entity.habit.HabitAchievementEntity;
+import com.depromeet.threedays.data.entity.habit.HabitEntity;
+import com.depromeet.threedays.front.domain.converter.habit.HabitAchievementConverter;
+import com.depromeet.threedays.front.domain.converter.habit.HabitConverter;
 import com.depromeet.threedays.front.domain.model.habit.Habit;
+import com.depromeet.threedays.front.domain.model.habit.HabitAchievement;
+import com.depromeet.threedays.front.exception.ResourceNotFoundException;
 import com.depromeet.threedays.front.repository.RewardHistoryRepository;
 import com.depromeet.threedays.front.repository.habit.HabitAchievementRepository;
 import com.depromeet.threedays.front.repository.habit.HabitRepository;
@@ -19,6 +25,27 @@ public class DeleteHabitAchievementUseCase {
     private final RewardHistoryRepository rewardHistoryRepository;
 
     public Habit execute(Long habitId, Long habitAchievementId) {
+        HabitEntity habitEntity = habitRepository.findById(habitId)
+                                                 .orElseThrow(ResourceNotFoundException::new);
+        HabitAchievementEntity habitAchievementEntity = habitAchievementRepository.findById(habitAchievementId)
+                                                                                  .orElseThrow(ResourceNotFoundException::new);
+        LocalDate achievementDate = habitAchievementEntity.getAchievementDate();
+        LocalDate requestDate = LocalDate.now();
 
+        if(!achievementDate.isEqual(requestDate)) {
+            return null;
+        }
+
+        habitAchievementRepository.deleteById(habitAchievementId);
+        HabitAchievementEntity beforeHabitAchievementEntity = habitAchievementRepository.findFirstByHabitIdOrderByAchievementDateDesc(habitId)
+                                                                             .orElseThrow(ResourceNotFoundException::new);
+        HabitAchievement habitAchievement = HabitAchievementConverter.from(beforeHabitAchievementEntity);
+
+        Long totalReward = rewardHistoryRepository.countByHabitId(habitId);
+        if (habitAchievementEntity.getSequence() % 3 == 0) {
+            rewardHistoryRepository.deleteFirstByHabitIdOrderByCreateDateDesc(habitId);
+            return HabitConverter.from(habitEntity, habitAchievement, totalReward - 1);
+        }
+        return HabitConverter.from(habitEntity, habitAchievement, totalReward);
     }
 }
