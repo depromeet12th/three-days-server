2ba238c84e5ea836229374847c2e0ab6864fb05c
saint6839 <saint6839@gmail.com>
2022-11-17 21:29:10 +0900

    DEP-138 feat : 달성을 취소하고자 하는 습관의 리워드 이력에서 가장 최근의 리워드 이력을 삭제하는 쿼리 메서드 추가

--- app/src/main/java/com/depromeet/threedays/front/repository/RewardHistoryRepository.java
+++ app/src/main/java/com/depromeet/threedays/front/repository/RewardHistoryRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RewardHistoryRepository extends JpaRepository<RewardHistoryEntity, Long> {
 
 	Long countByHabitId(Long habitId);
+
+	void deleteFirstByHabitIdOrderByCreateDateDesc(final Long habitId);
 }
