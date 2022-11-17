603c97ef917a2394c55a9f760d21cb35be8d89f2
saint6839 <saint6839@gmail.com>
2022-11-17 15:15:13 +0900

    DEP-124 feat : 이전 습관 달성 이력이 없을 경우 예외를 처리하고 sequence 1로 습관 달성을 생성하도록 구현

--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/habit/SaveHabitAchievementUseCase.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/habit/SaveHabitAchievementUseCase.java
@@ -1,53 +1,79 @@
 package com.depromeet.threedays.front.domain.usecase.habit;
 
+import com.depromeet.threedays.data.entity.habit.HabitAchievementEntity;
 import com.depromeet.threedays.data.entity.habit.HabitEntity;
 import com.depromeet.threedays.data.enums.DayOfWeek;
 import com.depromeet.threedays.front.controller.request.habit.SaveHabitAchievementRequest;
+import com.depromeet.threedays.front.domain.converter.habit.HabitAchievementConverter;
+import com.depromeet.threedays.front.domain.converter.habit.HabitConverter;
 import com.depromeet.threedays.front.domain.model.habit.Habit;
+import com.depromeet.threedays.front.domain.model.habit.HabitAchievement;
+import com.depromeet.threedays.front.exception.ResourceNotFoundException;
 import com.depromeet.threedays.front.repository.habit.HabitAchievementRepository;
 import com.depromeet.threedays.front.repository.habit.HabitRepository;
+import java.time.LocalDate;
+import java.util.EnumSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.util.EnumSet;
 
 @Component
 @Transactional
 @RequiredArgsConstructor
 public class SaveHabitAchievementUseCase {
-    private final HabitRepository habitRepository;
-    private final HabitAchievementRepository habitAchievementRepository;
+	private final HabitRepository habitRepository;
+	private final HabitAchievementRepository habitAchievementRepository;
 
-    public Habit execute(Long habitId, final SaveHabitAchievementRequest request) {
-        return null;
-    }
+	public Habit execute(Long habitId, final SaveHabitAchievementRequest request) {
 
-    private int calculateDayDifference(HabitEntity habitEntity, LocalDate achievementDate) {
-        EnumSet<DayOfWeek> dayOfWeeks = habitEntity.getDayOfWeeks();
-        int achievementDayOfWeek = achievementDate.getDayOfWeek()
-                                                  .getValue();
+		LocalDate achievementDate = request.getAchievementDate();
+		HabitEntity habitEntity =
+				habitRepository.findById(habitId).orElseThrow(ResourceNotFoundException::new);
 
-        int count = 0;
-        int dayDifference = 0;
+		HabitAchievementEntity habitAchievementEntity;
+		try {
+			habitAchievementEntity =
+					habitAchievementRepository
+							.findFirstByHabitIdOrderByAchievementDateDesc(habitId)
+							.orElseThrow(ResourceNotFoundException::new);
+		} catch (ResourceNotFoundException e) {
+			int dayDifference = calculateDayDifference(habitEntity, achievementDate);
 
-        for (DayOfWeek dayOfWeek : dayOfWeeks) {
-            int objectiveDayOfWeek = dayOfWeek.getValue();
-            if (achievementDayOfWeek < objectiveDayOfWeek) {
-                dayDifference = objectiveDayOfWeek - achievementDayOfWeek;
-                break;
-            }
-            count++;
-        }
+			HabitAchievementEntity savedHabitAchievementEntity =
+					habitAchievementRepository.save(
+							HabitAchievementConverter.to(habitEntity, request, dayDifference));
 
-        if (count == dayOfWeeks.size()) {
-            for (DayOfWeek dayOfWeek : dayOfWeeks) {
-                int habitDayOfWeek = dayOfWeek.getValue();
-                dayDifference = (habitDayOfWeek + 7) - achievementDayOfWeek;
-                break;
-            }
-        }
-        return dayDifference;
-    }
+			HabitAchievement habitAchievement =
+					HabitAchievementConverter.from(savedHabitAchievementEntity);
+			return HabitConverter.from(habitEntity, habitAchievement, 3L);
+		}
+
+		return null;
+	}
+
+	private int calculateDayDifference(HabitEntity habitEntity, LocalDate achievementDate) {
+		EnumSet<DayOfWeek> dayOfWeeks = habitEntity.getDayOfWeeks();
+		int achievementDayOfWeek = achievementDate.getDayOfWeek().getValue();
+
+		int count = 0;
+		int dayDifference = 0;
+
+		for (DayOfWeek dayOfWeek : dayOfWeeks) {
+			int objectiveDayOfWeek = dayOfWeek.getValue();
+			if (achievementDayOfWeek < objectiveDayOfWeek) {
+				dayDifference = objectiveDayOfWeek - achievementDayOfWeek;
+				break;
+			}
+			count++;
+		}
+
+		if (count == dayOfWeeks.size()) {
+			for (DayOfWeek dayOfWeek : dayOfWeeks) {
+				int habitDayOfWeek = dayOfWeek.getValue();
+				dayDifference = (habitDayOfWeek + 7) - achievementDayOfWeek;
+				break;
+			}
+		}
+		return dayDifference;
+	}
 }
