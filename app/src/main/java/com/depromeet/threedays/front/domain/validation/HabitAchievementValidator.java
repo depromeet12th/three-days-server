69a6aa4a7d737fcc774cca0b8bbd840c3d5e7763
saint6839 <saint6839@gmail.com>
2022-11-17 22:29:24 +0900

    DEP-138 feat : 습관 달성 취소에 대한 Validator 추가
    - 습관 달성 취소 요청 일자와 오늘 날짜가 같은지 검증

---
+++ app/src/main/java/com/depromeet/threedays/front/domain/validation/HabitAchievementValidator.java
@@ -0,0 +1,35 @@
+package com.depromeet.threedays.front.domain.validation;
+
+import com.depromeet.threedays.data.entity.habit.HabitAchievementEntity;
+import com.depromeet.threedays.front.exception.PolicyViolationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class HabitAchievementValidator {
+
+    public void validateCancelDateConstraints(final HabitAchievementEntity entity) {
+        this.throwIfNotEqualToday(entity.getAchievementDate());
+    }
+
+    private void throwIfNotEqualToday(final LocalDate achievementDate) {
+        if (achievementDate == null) {
+            return;
+        }
+
+        final String INVALID_REQUEST_DATE = "invalid.request.date";
+        this.throwIf(achievementDate.isEqual(LocalDate.now()), INVALID_REQUEST_DATE);
+    }
+
+    private void throwIf(final boolean condition, final String messageCodeSuffix) {
+        final String MESSAGE_CODE_PREFIX = "habit.achievement.constraints.";
+
+        if(condition) {
+            return;
+        }
+        throw new PolicyViolationException(MESSAGE_CODE_PREFIX + messageCodeSuffix);
+    }
+}
