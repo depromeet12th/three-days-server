51eb30553533646f30810491bf02545ab4f19b2d
saint6839 <saint6839@gmail.com>
2022-11-24 14:18:35 +0900

    DEP-125 test : 습관과 연결되어 있는 짝꿍이 있을 때, 삭제 요청이 들어오면 짝꿍이 잘 삭제 되는지 검증하는 테스트 추가

---
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitUseCaseSpec.groovy
@@ -0,0 +1,51 @@
+package com.depromeet.threedays.front.domain.usecase.habit
+
+
+import com.depromeet.threedays.front.IntegrationTestSpecification
+import com.depromeet.threedays.front.data.habit.HabitAchievementDataInitializer
+import com.depromeet.threedays.front.data.habit.HabitDataInitializer
+import com.depromeet.threedays.front.data.mate.MateDataInitializer
+import com.depromeet.threedays.front.persistence.repository.habit.HabitRepository
+import com.depromeet.threedays.front.persistence.repository.mate.MateRepository
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Subject
+
+class DeleteHabitUseCaseSpec extends IntegrationTestSpecification {
+
+    @Subject
+    @Autowired
+    private DeleteHabitUseCase deleteUseCase
+
+    @Autowired
+    private HabitDataInitializer habitDataInitializer
+
+    @Autowired
+    private HabitAchievementDataInitializer habitAchievementDataInitializer
+
+    @Autowired
+    private MateDataInitializer mateDataInitializer
+
+    @Autowired
+    private HabitRepository repository
+
+    @Autowired
+    private MateRepository mateRepository
+
+    def setup() {
+        habitDataInitializer.initialize()
+    }
+
+    def "짝꿍과 연결되어 있는 습관에 삭제 요청이 들어왔을 때 Mate가 삭제되는지 테스트"() {
+        given:
+        def habitData = habitDataInitializer.data.first()
+        mateDataInitializer.initialize(habitData.id)
+        def mateData = mateDataInitializer.getData()
+
+        when:
+        deleteUseCase.execute(habitData.id)
+        def result = mateRepository.findById(mateData.id).get()
+
+        then:
+        result.deleted == true
+    }
+}
