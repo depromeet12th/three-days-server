1d0bc684ca470ab601de22485777938bf62b7861
saint6839 <saint6839@gmail.com>
2022-11-24 13:58:32 +0900

    DEP-125 feat : 짝꿍 테스트를 위한 FakeMateEntity 추가

---
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/data/mate/FakeMateEntity.java
@@ -0,0 +1,20 @@
+package com.depromeet.threedays.front.data.mate;
+
+import com.depromeet.threedays.data.entity.mate.MateEntity;
+import net.bytebuddy.utility.RandomString;
+
+import java.time.LocalDateTime;
+
+public class FakeMateEntity {
+
+    public static MateEntity create(Long habitId) {
+        return MateEntity.builder()
+                         .deleted(false)
+                         .createAt(LocalDateTime.now())
+                         .memberId(0L)
+                         .habitId(habitId)
+                         .characterType(RandomString.make())
+                         .level(3)
+                         .build();
+    }
+}
