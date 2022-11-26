80ce19cb2fe6b9b8de2de9526679d1f31f98ded8
saint6839 <saint6839@gmail.com>
2022-11-26 13:29:31 +0900

    DEP-125 test : 습관 달성 이력이 있을 때 삭제 요쳥이 들어오면 보관함으로 이동 되는지 테스트 작성

--- app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitUseCaseSpec.groovy
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitUseCaseSpec.groovy
@@ -1,5 +1,8 @@
 package com.depromeet.threedays.front.domain.usecase.habit
 
+import com.depromeet.threedays.data.entity.habit.HabitAchievementEntity
+import com.depromeet.threedays.data.entity.habit.HabitEntity
+import com.depromeet.threedays.data.enums.HabitStatus
 import com.depromeet.threedays.front.IntegrationTestSpecification
 import com.depromeet.threedays.front.data.habit.HabitAchievementDataInitializer
 import com.depromeet.threedays.front.data.habit.HabitDataInitializer
@@ -58,6 +61,19 @@ class DeleteHabitUseCaseSpec extends IntegrationTestSpecification {
 
         then:
         result.deleted == true
+    }
 
+    def "습관 달성 이력이 있는 습관에 삭제 요청이 들어왔을 때 습관이 보관함으로 이동되는지 테스트"() {
+        given:
+        def habitData = habitDataInitializer.data.first()
+        habitAchievementDataInitializer.initialize(habitData.id)
+        def habitAchievementData = habitAchievementDataInitializer.data.first()
+
+        when:
+        deleteUseCase.execute(habitData.id)
+        def result = repository.findById(habitData.id).get()
+
+        then:
+        result.status == HabitStatus.ARCHIVED
     }
 }
