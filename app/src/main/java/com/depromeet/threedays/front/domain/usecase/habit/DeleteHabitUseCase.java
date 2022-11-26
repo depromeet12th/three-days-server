518be8b06b2113508d70abf353a1068f1680eac0
saint6839 <saint6839@gmail.com>
2022-11-26 13:11:21 +0900

    DEP-125 feat : Habit 보관함 이동, 완전 삭제 기능 추가
    - 습관 달성 이력이 없을 경우 -> 완전 삭제
    - 짝꿍이 있을 경우 -> 보관함 이동, 짝꿍 완전 삭제
    - 습관 상태가 Active일 경우 -> 보관함 이동
    - 습관 상태가 Archived일 경우 -> 오나전 삭제

--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitUseCase.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/habit/DeleteHabitUseCase.java
@@ -1,11 +1,13 @@
 package com.depromeet.threedays.front.domain.usecase.habit;
 
+import com.depromeet.threedays.data.entity.habit.HabitAchievementEntity;
 import com.depromeet.threedays.data.entity.habit.HabitEntity;
 import com.depromeet.threedays.data.entity.mate.MateEntity;
-import com.depromeet.threedays.front.domain.converter.habit.HabitConverter;
-import com.depromeet.threedays.front.domain.converter.mate.MateConverter;
-import com.depromeet.threedays.front.domain.model.habit.Habit;
+import com.depromeet.threedays.data.enums.HabitStatus;
+import com.depromeet.threedays.front.domain.converter.habit.HabitAchievementConverter;
+import com.depromeet.threedays.front.domain.model.habit.HabitAchievement;
 import com.depromeet.threedays.front.exception.ResourceNotFoundException;
+import com.depromeet.threedays.front.persistence.repository.habit.HabitAchievementRepository;
 import com.depromeet.threedays.front.persistence.repository.habit.HabitRepository;
 import com.depromeet.threedays.front.persistence.repository.mate.MateRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,25 +20,46 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class DeleteHabitUseCase {
     private final HabitRepository repository;
+    private final HabitAchievementRepository habitAchievementRepository;
     private final MateRepository mateRepository;
 
     public void execute(Long habitId) {
         HabitEntity entity = repository.findById(habitId)
-                                            .orElseThrow(ResourceNotFoundException::new);
-        MateEntity mateEntity = mateRepository.findByHabitId(HabitConverter.from(entity)
-                                                                           .getHabitId())
-                                              .orElseThrow(ResourceNotFoundException::new);
+                                       .orElseThrow(ResourceNotFoundException::new);
+        MateEntity mateEntity = mateRepository.findByHabitId(habitId)
+                                              .orElse(null);
+        HabitAchievementEntity habitAchievementEntity = habitAchievementRepository.findFirstByHabitIdOrderByAchievementDateDesc(habitId)
+                                                                                  .orElse(null);
 
-        Habit target = HabitConverter.from(entity, MateConverter.from(mateEntity));
+        HabitAchievement habitAchievement = HabitAchievementConverter.from(habitAchievementEntity);
 
-        deleteAssociation(target);
+        this.delete(entity, mateEntity, habitAchievement);
     }
 
-    private void deleteAssociation(Habit target) {
-        if (target != null) {
-            mateRepository.findByHabitId(target.getHabitId())
-                          .orElseThrow(ResourceNotFoundException::new)
-                          .deleteMate();
+    private void delete(HabitEntity entity, MateEntity mateEntity, HabitAchievement habitAchievement) {
+        if (habitAchievement == null) {
+            entity.deleteHabit();
         }
+
+        if (mateEntity != null) {
+            entity.changeStatusToArchived();
+            this.deleteAssociation(mateEntity);
+            return;
+        }
+
+        if (entity.getStatus()
+                  .equals(HabitStatus.ACTIVE)) {
+            entity.changeStatusToArchived();
+            return;
+        }
+
+        if (entity.getStatus()
+                  .equals(HabitStatus.ARCHIVED)) {
+            entity.deleteHabit();
+        }
+    }
+
+    private void deleteAssociation(MateEntity mateEntity) {
+        mateEntity.deleteMate();
     }
 }
