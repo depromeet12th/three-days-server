c0f51f9bd1211138d0ed778c84e65c417591e236
saint6839 <saint6839@gmail.com>
2022-11-27 19:45:28 +0900

    DEP-125 feat : HabitRepository 완전 삭제 여부, 보관함 이동 여부 update jpql 추가

--- app/src/main/java/com/depromeet/threedays/front/persistence/repository/habit/HabitRepository.java
+++ app/src/main/java/com/depromeet/threedays/front/persistence/repository/habit/HabitRepository.java
@@ -2,9 +2,21 @@ package com.depromeet.threedays.front.persistence.repository.habit;
 
 import com.depromeet.threedays.data.entity.habit.HabitEntity;
 import java.util.List;
+
+import com.depromeet.threedays.data.enums.HabitStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HabitRepository extends JpaRepository<HabitEntity, Long> {
 
 	List<HabitEntity> findAllByMemberIdAndDeletedFalse(final Long memberId);
+
+	@Modifying(clearAutomatically = true)
+	@Query("update HabitEntity h set h.deleted = :deleted where  h.id = :habitId")
+	void updateDeletedById(final Long habitId, final Boolean deleted);
+
+	@Modifying(clearAutomatically = true)
+	@Query("update HabitEntity h set h.status = :status where h.id = :habitId")
+	void updateStatusById(final Long habitId, final HabitStatus status);
 }
