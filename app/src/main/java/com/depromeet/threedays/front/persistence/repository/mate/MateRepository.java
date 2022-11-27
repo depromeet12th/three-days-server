7e9375f98a79cf2fd69ba6d119e6040f7b2d2d87
saint6839 <saint6839@gmail.com>
2022-11-27 19:45:41 +0900

    DEP-125 feat : MateRepository 완전 삭제 jpql 추가

--- app/src/main/java/com/depromeet/threedays/front/persistence/repository/mate/MateRepository.java
+++ app/src/main/java/com/depromeet/threedays/front/persistence/repository/mate/MateRepository.java
@@ -3,8 +3,14 @@ package com.depromeet.threedays.front.persistence.repository.mate;
 import com.depromeet.threedays.data.entity.mate.MateEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MateRepository extends JpaRepository<MateEntity, Long> {
 
 	Optional<MateEntity> findByHabitId(final Long habitId);
+
+	@Modifying(clearAutomatically = true)
+	@Query("update MateEntity m set m.deleted = :deleted where  m.habitId = :habitId")
+	void updateDeletedById(final Long habitId, final Boolean deleted);
 }
