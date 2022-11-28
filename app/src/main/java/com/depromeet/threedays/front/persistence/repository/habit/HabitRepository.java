118682c6b4d908bbb2ff2f0810344a654b06cf45
saint6839 <saint6839@gmail.com>
2022-11-28 15:29:02 +0900

    DEP-125 style : apply lint

--- app/src/main/java/com/depromeet/threedays/front/persistence/repository/habit/HabitRepository.java
+++ app/src/main/java/com/depromeet/threedays/front/persistence/repository/habit/HabitRepository.java
@@ -1,11 +1,8 @@
 package com.depromeet.threedays.front.persistence.repository.habit;
 
 import com.depromeet.threedays.data.entity.habit.HabitEntity;
-import com.depromeet.threedays.data.enums.HabitStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface HabitRepository extends JpaRepository<HabitEntity, Long> {
 
