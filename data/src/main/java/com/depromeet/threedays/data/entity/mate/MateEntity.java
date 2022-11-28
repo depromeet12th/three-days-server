9edd018755f96a1d4951f7d4fc1fcb08dd0e0f2d
saint6839 <saint6839@gmail.com>
2022-11-28 17:29:57 +0900

    DEP-125 style : lint apply

--- data/src/main/java/com/depromeet/threedays/data/entity/mate/MateEntity.java
+++ data/src/main/java/com/depromeet/threedays/data/entity/mate/MateEntity.java
@@ -8,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
