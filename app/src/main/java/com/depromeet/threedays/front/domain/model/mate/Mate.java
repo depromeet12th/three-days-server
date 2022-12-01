882832cb840ad79e9951c724f00868cf036850cc
saint6839 <saint6839@gmail.com>
2022-12-01 15:08:51 +0900

    DEP-171 feat : Mate response api spec에 title 추가

--- app/src/main/java/com/depromeet/threedays/front/domain/model/mate/Mate.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/model/mate/Mate.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class Mate implements Serializable {
 
 	private Long id;
+	private String title;
 	private LocalDateTime createAt;
 	private Integer level;
 	private String characterType;
