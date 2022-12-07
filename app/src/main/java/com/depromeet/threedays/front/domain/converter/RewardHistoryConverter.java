b7ac30fe320a022c0ddf2e8a60a457986468bcd1
gojung <wjdrndk96@gmail.com>
2022-12-07 13:02:41 +0900

    DEP-180 lint

--- app/src/main/java/com/depromeet/threedays/front/domain/converter/RewardHistoryConverter.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/converter/RewardHistoryConverter.java
@@ -2,17 +2,18 @@ package com.depromeet.threedays.front.domain.converter;
 
 import com.depromeet.threedays.data.entity.history.RewardHistoryEntity;
 import com.depromeet.threedays.front.domain.model.habit.Habit;
-import java.time.LocalDateTime;
 import lombok.experimental.UtilityClass;
+
+import java.time.LocalDateTime;
 
 @UtilityClass
 public class RewardHistoryConverter {
 
-	public static RewardHistoryEntity to(Habit habit) {
-		return RewardHistoryEntity.builder()
-				.habitId(habit.getId())
-				.memberId(habit.getMemberId())
-				.createAt(LocalDateTime.now())
-				.build();
-	}
+    public static RewardHistoryEntity to(Habit habit) {
+        return RewardHistoryEntity.builder()
+                .habitId(habit.getId())
+                .memberId(habit.getMemberId())
+                .createAt(LocalDateTime.now())
+                .build();
+    }
 }
