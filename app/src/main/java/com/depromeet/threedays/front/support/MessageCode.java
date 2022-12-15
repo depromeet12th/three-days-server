c99104ec337a0a7f7470adc58d43fee39d30b501
gojung <wjdrndk96@gmail.com>
2022-12-15 22:47:06 +0900

    lint

--- app/src/main/java/com/depromeet/threedays/front/support/MessageCode.java
+++ app/src/main/java/com/depromeet/threedays/front/support/MessageCode.java
@@ -3,7 +3,7 @@ package com.depromeet.threedays.front.support;
 public enum MessageCode {
 	SUCCESS("success", "성공"),
 	RESOURCE_DELETED("resource.deleted", "삭제되었습니다."),
-	RESOURCE_CREATED("created", "새로 생성되었습니다.");
+	RESOURCE_CREATED("resource.created", "새로 생성되었습니다.");
 	private final String code;
 	private final String value;
 
