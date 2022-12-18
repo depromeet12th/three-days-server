b964f9c63327e21f75ed728c5c60bdabbedff450
saint6839 <saint6839@gmail.com>
2022-12-18 19:39:16 +0900

    DEP-214 feat : 예외 핸들링 테스트 작성
    HttpMediaTypeNotSupportedException
    MethodArgumentNotValidException
    HttpMessageNotReadableException
    HttpRequestMethodNotSupportedException

---
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/web/controller/HabitControllerSpec.groovy
@@ -0,0 +1,75 @@
+package com.depromeet.threedays.front.web.controller
+
+import com.depromeet.threedays.front.IntegrationTestSpecification
+import com.depromeet.threedays.front.web.request.member.MemberNameUpdateRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.hamcrest.Matchers
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+import java.time.DayOfWeek
+
+class HabitControllerSpec extends IntegrationTestSpecification {
+
+    @Autowired
+    private MockMvc mockMvc
+
+    @Autowired
+    private ObjectMapper objectMapper
+
+    def "HttpMediaTypeNotSupportedException 이 핸들링 되는지 테스트"() {
+        given:
+        when:
+        def resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/habits")
+                        .contentType(MediaType.TEXT_PLAIN)
+        )
+        then:
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath("\$.message").value("Content type 'text/plain;charset=UTF-8' not supported"))
+    }
+
+    def "MethodArgumentNotValidException 이 핸들링 되는지 테스트"() {
+        given:
+        def content = objectMapper.writeValueAsString(new MemberNameUpdateRequest("saint6839"))
+
+        when:
+        def resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/habits")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        then:
+        resultActions.andExpect(MockMvcResultMatchers.status().is4xxClientError())
+    }
+
+    def "HttpMessageNotReadableException 이 핸들링 되는지 테스트"() {
+        given:
+        when:
+        def resultActions = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/habits/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+
+        then:
+        resultActions.andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Required request body is missing")))
+    }
+
+    def "HttpRequestMethodNotSupportedException 이 핸들링 되는지 테스트"() {
+        given:
+        def dayOfWeeks = EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY,
+                DayOfWeek.SUNDAY);
+
+        when:
+        def resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/habits/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+
+        then:
+        resultActions.andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Request method 'POST' not supported")))
+    }
+}
