9b4fa8a63f1663a981f5e976b8bd7d2a34263ec6
saint6839 <saint6839@gmail.com>
2022-12-21 23:08:53 +0900

    DEP-241 style : lint applied

--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/mate/GetMateCheckUseCase.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/mate/GetMateCheckUseCase.java
@@ -2,11 +2,9 @@ package com.depromeet.threedays.front.domain.usecase.mate;
 
 import com.depromeet.threedays.front.config.security.AuditorHolder;
 import com.depromeet.threedays.front.domain.converter.mate.MateConverter;
-import com.depromeet.threedays.front.domain.model.mate.Mate;
 import com.depromeet.threedays.front.persistence.repository.mate.MateRepository;
 import com.depromeet.threedays.front.web.response.MateResponse;
 import com.depromeet.threedays.front.web.response.converter.MateResponseConverter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -21,11 +19,9 @@ public class GetMateCheckUseCase {
 	private final MateRepository repository;
 
 	public List<MateResponse> execute() {
-		return repository.findByMemberIdAndDeletedFalse(AuditorHolder.get())
-						  .stream()
-						  .map(MateConverter::from)
-						  .map(MateResponseConverter::from)
-						  .collect(Collectors.toList());
-
+		return repository.findByMemberIdAndDeletedFalse(AuditorHolder.get()).stream()
+				.map(MateConverter::from)
+				.map(MateResponseConverter::from)
+				.collect(Collectors.toList());
 	}
 }
