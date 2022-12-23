c8c6fbd29e24623316375d685dd55b7377ab4548
Haeseong Jeon <seong0428@gmail.com>
2022-12-24 04:15:06 +0900

    refactor: 알림 발송, 이력 조회 controller 분리

--- app/src/main/java/com/depromeet/threedays/front/web/controller/NotificationController.java
+++ app/src/main/java/com/depromeet/threedays/front/web/controller/NotificationController.java
@@ -1,61 +1,36 @@
 package com.depromeet.threedays.front.web.controller;
 
-import com.depromeet.threedays.front.domain.usecase.notification.SaveNotificationUseCase;
-import com.depromeet.threedays.front.domain.usecase.notification.SearchNotificationUseCase;
 import com.depromeet.threedays.front.domain.usecase.notification.SendGlobalNotificationUseCase;
 import com.depromeet.threedays.front.domain.usecase.notification.SendHabitNotificationUseCase;
 import com.depromeet.threedays.front.support.ApiResponse;
 import com.depromeet.threedays.front.support.ApiResponseGenerator;
-import com.depromeet.threedays.front.web.request.notification.EditStatusNotificationRequest;
 import com.depromeet.threedays.front.web.response.NotificationBatchResponse;
-import com.depromeet.threedays.front.web.response.NotificationHistoryResponse;
 import com.google.firebase.messaging.BatchResponse;
 import java.util.List;
-import java.util.stream.Collectors;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/notifications")
 @RequiredArgsConstructor
 @RestController
 public class NotificationController {
+	private final SendGlobalNotificationUseCase sendGlobalNotificationUseCase;
+	private final SendHabitNotificationUseCase sendHabitNotificationUseCase;
 
-	private final SendGlobalNotificationUseCase globalUseCase;
-	private final SendHabitNotificationUseCase habitUseCase;
-
-	private final SearchNotificationUseCase searchUseCase;
-
-	private final SaveNotificationUseCase saveUseCase;
-
-	@GetMapping
-	public ApiResponse<ApiResponse.SuccessBody<List<NotificationHistoryResponse>>> browse() {
-		return ApiResponseGenerator.success(
-				searchUseCase.execute().stream()
-						.map(NotificationHistoryResponse::from)
-						.collect(Collectors.toList()),
-				HttpStatus.OK);
-	}
-
-	@PatchMapping("/{notificationHistoryId}")
-	public ApiResponse<ApiResponse.SuccessBody<NotificationHistoryResponse>> editStatus(
-			@PathVariable final Long notificationHistoryId,
-			@RequestBody @Valid final EditStatusNotificationRequest request) {
-		return ApiResponseGenerator.success(
-				NotificationHistoryResponse.from(saveUseCase.execute(notificationHistoryId, request)),
-				HttpStatus.OK);
-	}
-
+	// FIXME: 관리자 권한 검증 필요
 	@PostMapping("/global")
 	public ApiResponse<ApiResponse.SuccessBody<List<NotificationBatchResponse>>>
 			sendGlobalNotification() {
-		return ApiResponseGenerator.success(globalUseCase.execute(), HttpStatus.OK);
+		return ApiResponseGenerator.success(sendGlobalNotificationUseCase.execute(), HttpStatus.OK);
 	}
 
+	// FIXME: 관리자 권한 검증 필요
 	@PostMapping("/habit")
 	public ApiResponse<ApiResponse.SuccessBody<List<BatchResponse>>> sendHabitNotification() {
-		return ApiResponseGenerator.success(habitUseCase.execute(), HttpStatus.OK);
+		return ApiResponseGenerator.success(sendHabitNotificationUseCase.execute(), HttpStatus.OK);
 	}
 
 	@PostMapping("/test")
