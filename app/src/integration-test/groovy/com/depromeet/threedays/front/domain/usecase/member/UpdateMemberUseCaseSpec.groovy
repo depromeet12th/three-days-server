ec547a90377469a8b8b2b2fe7ac3923c4825c6ec
saint6839 <saint6839@gmail.com>
2022-12-27 22:31:37 +0900

    DEP-300 refactor : 테스트 코드 통합

--- app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/member/UpdateMemberUseCaseSpec.groovy
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/member/UpdateMemberUseCaseSpec.groovy
@@ -4,32 +4,39 @@ import com.depromeet.threedays.data.enums.MemberStatus
 import com.depromeet.threedays.front.IntegrationTestSpecification
 import com.depromeet.threedays.front.data.member.MemberInitializer
 import com.depromeet.threedays.front.persistence.repository.member.MemberRepository
-import com.depromeet.threedays.front.web.request.member.MemberNameUpdateRequest
+import com.depromeet.threedays.front.web.request.member.MemberUpdateRequest
+import org.json.simple.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 
-class SaveMemberNameUseCaseSpec extends IntegrationTestSpecification {
+
+class UpdateMemberUseCaseSpec extends IntegrationTestSpecification {
 
     @Autowired
     MemberInitializer initializer
     MemberRepository mockRepo
-    SaveNameUseCase useCase
+    UpdateMemberUseCase useCase
 
     def setup() {
-        mockRepo = Mock(MemberRepository.class)
-        useCase = new SaveNameUseCase(mockRepo)
         initializer.initialize()
+        mockRepo = Mock(MemberRepository.class)
+        useCase = new UpdateMemberUseCase(mockRepo)
     }
 
-    def "사용자는 이름을 새로 등록할 수 있다"() {
+    def "사용자는 name 데이터를 업데이트 할 수 있다"() {
         setup:
         def id = initializer.data[2]
-        def expected = MemberNameUpdateRequest.builder().name("admin").build()
+
+        def json = new JSONObject()
+        json.put("test", "test")
+        def expected = MemberUpdateRequest.builder().resource(json).name("admin").notificationConsent(false).build()
 
         when:
         def actual = useCase.execute(expected)
 
         then:
         mockRepo.findByIdAndStatus(_ as Long, _ as MemberStatus) >> Optional.of(id)
+        actual.resource.get("test") == json.get("test")
         actual.name == expected.name
+        !actual.notificationConsent
     }
 }
