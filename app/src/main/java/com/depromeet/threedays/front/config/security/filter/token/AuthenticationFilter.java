53c3762897a4b44e6062ed35143588fd626bab5a
Haeseong Jeon <seong0428@gmail.com>
2022-12-28 15:01:49 +0900

    fix: accessToken 유효하지 않으면 401 응답하도록 변경

--- app/src/main/java/com/depromeet/threedays/front/config/security/filter/token/AuthenticationFilter.java
+++ app/src/main/java/com/depromeet/threedays/front/config/security/filter/token/AuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.depromeet.threedays.front.config.security.filter.token;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,15 +11,24 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
 @RequiredArgsConstructor
 public class AuthenticationFilter extends AbstractPreAuthenticatedProcessingFilter {
 
-	private final TokenResolver tokenResolver;
+	private static final Pattern PATTERN_AUTHORIZATION_HEADER = Pattern.compile("^[Bb]earer (.*)$");
 
 	@Override
 	protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-		return tokenResolver.resolveToken(request);
+		return resolveAccessToken(request);
 	}
 
 	@Override
 	protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
-		return tokenResolver.resolveToken(request);
+		return resolveAccessToken(request);
+	}
+
+	private String resolveAccessToken(HttpServletRequest request) {
+		String authorization = request.getHeader("Authorization");
+		Matcher matcher = PATTERN_AUTHORIZATION_HEADER.matcher(authorization);
+		if (!matcher.matches()) {
+			return null;
+		}
+		return matcher.group(1);
 	}
 }
