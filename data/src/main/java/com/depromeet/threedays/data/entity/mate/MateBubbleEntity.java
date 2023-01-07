4fa919ab5035f901d94567af0a24534d71d858b6
saint6839 <saint6839@gmail.com>
2023-01-07 16:44:37 +0900

    DEP-360 style : lint applied

--- data/src/main/java/com/depromeet/threedays/data/entity/mate/MateBubbleEntity.java
+++ data/src/main/java/com/depromeet/threedays/data/entity/mate/MateBubbleEntity.java
@@ -1,13 +1,12 @@
 package com.depromeet.threedays.data.entity.mate;
 
 import com.depromeet.threedays.data.enums.MateType;
+import java.time.LocalDateTime;
 import javax.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
