ef5b7859ff2f03c12bc8d5041029bfb7d805d3f1
gojung <wjdrndk96@gmail.com>
2023-01-08 20:58:45 +0900

    dep-359 fix: post client docs

--- app/src/integration-test/groovy/com/depromeet/threedays/front/web/controller/docs/ClientControllerDocsSpec.groovy
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/web/controller/docs/ClientControllerDocsSpec.groovy
@@ -5,17 +5,22 @@ import com.depromeet.threedays.front.domain.model.client.Client
 import com.depromeet.threedays.front.domain.usecase.client.SaveClientUseCase
 import com.depromeet.threedays.front.web.controller.ClientController
 import com.depromeet.threedays.front.web.request.client.ClientRequest
+import com.epages.restdocs.apispec.ResourceSnippetDetails
+import com.epages.restdocs.apispec.ResourceSnippetParameters
+import com.epages.restdocs.apispec.Schema
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
+import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*
 import static org.springframework.restdocs.payload.PayloadDocumentation.*
@@ -39,6 +44,8 @@ class ClientControllerDocsSpec extends RestDocsSpecification {
                 .fcmToken('fcmToken').build()
     }
 
+    private static final TAG = "Client"
+
     def 'generate postClients'() {
         given:
         def request = new ClientRequest("fcmToken here", "identificationKey here")
@@ -51,17 +58,19 @@ class ClientControllerDocsSpec extends RestDocsSpecification {
                 .andExpect(status().is2xxSuccessful())
                 .andDo(
                         document("postClients",
-                                "save client",
-                                "클라이언트 정보 추가시 사용하는 API. fcm-token 추가 등록/갱신 시 해당 API 사용",
-                                false,
-                                false,
-                                preprocessRequest(prettyPrint()),
-                                preprocessResponse(prettyPrint()),
-                                requestFields(
-                                        fieldWithPath('fcmToken').description('device fcmToken'),
-                                        fieldWithPath("identificationKey").description("device identifier")
-                                ),
-                                responseFields(Descriptor.client()) & Descriptor.successResponse()
+                                resource(
+                                        ResourceSnippetParameters.builder()
+                                                .description("클라이언트 정보 추가시 사용하는 API. fcm-token 추가 등록/갱신 시 해당 API 사용")
+                                                .tag(TAG)
+                                                .requestSchema(Schema.schema("SaveClientRequest"))
+                                                .responseSchema(Schema.schema("Client"))
+                                                .requestFields(
+                                                        fieldWithPath("fcmToken").type(JsonFieldType.STRING).description("fcm token!!!!"),
+                                                        fieldWithPath("identificationKey").type(JsonFieldType.STRING).description("identification key!!!"),
+                                                )
+                                                .responseFields(Descriptor.successResponse(Descriptor.client()))
+                                                .build()
+                                )
                         )
                 )
 
