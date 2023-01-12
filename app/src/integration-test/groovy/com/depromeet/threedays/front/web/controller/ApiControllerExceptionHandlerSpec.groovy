cc3af976df93667a6307ea56ccddb7de3ff50c90
gojung <wjdrndk96@gmail.com>
2023-01-12 21:50:20 +0900

    fix: exception handler message 수정

---
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/web/controller/ApiControllerExceptionHandlerSpec.groovy
@@ -0,0 +1,143 @@
+package com.depromeet.threedays.front.web.controller
+
+import com.depromeet.threedays.front.IntegrationTestSpecification
+import com.depromeet.threedays.front.domain.command.SaveMemberCommand
+import com.depromeet.threedays.front.domain.usecase.habit.DeleteHabitAchievementUseCase
+import com.depromeet.threedays.front.domain.usecase.habit.SaveHabitUseCase
+import com.depromeet.threedays.front.domain.usecase.mate.SaveMateUseCase
+import com.depromeet.threedays.front.domain.usecase.member.SaveMemberUseCase
+import com.depromeet.threedays.front.exception.ExternalIntegrationException
+import com.depromeet.threedays.front.exception.PolicyViolationException
+import com.depromeet.threedays.front.support.TokenGenerator
+import com.depromeet.threedays.front.web.request.habit.SaveHabitRequest
+import com.depromeet.threedays.front.web.request.mate.SaveMateRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.MockMvc
+
+class ApiControllerExceptionHandlerSpec extends IntegrationTestSpecification {
+
+    @Autowired
+    private MockMvc mockMvc
+
+    @Autowired
+    private ObjectMapper objectMapper
+
+    @Autowired
+    private TokenGenerator tokenGenerator
+
+    private String habit_constraints_insufficient_day_of_weeks = "실천 요일을 3개 이상 선택해 주세요."
+    private String habit_achievement_constraints_date_is_in_the_past = "이전의 기록을 변경할 수 없어요."
+    private String habit_achievement_constraints_invalid_achievement_date = "습관 취소는 오늘만 할 수 있어요."
+    private String habit_constraints_insufficient_notification = "Push 알림을 마저 설정해 주세요."
+    private String social_login_error = "로그인에 실패했어요."
+    private String mate_constraints_already_exist_mate = "이미 함께하고 있는 짝꿍이 있어요."
+    private String mate_constraints_invalid_habit = "생성된 습관이 없을 때는 짝꿍을 만들 수 없어요!"
+
+    private String token
+
+    def saveHabitUseCase = Mock(SaveHabitUseCase)
+    def deleteHabitAchievementUseCase = Mock(DeleteHabitAchievementUseCase)
+    def saveMemberUseCase = Mock(SaveMemberUseCase)
+    def saveMateUseCase = Mock(SaveMateUseCase)
+
+    def "habit.constraints.insufficient.day.of.weeks"() {
+        given:
+        saveHabitUseCase.execute(_ as SaveHabitRequest) >> {
+            SaveHabitRequest request -> throw new PolicyViolationException("habit.constraints.insufficient.day.of.weeks")
+        }
+
+        when:
+        saveHabitUseCase.execute(new SaveHabitRequest())
+
+        then:
+        def e = thrown(PolicyViolationException)
+        e.message == habit_constraints_insufficient_day_of_weeks
+    }
+
+    def "habit.constraints.insufficient.notification"() {
+        given:
+        saveHabitUseCase.execute(_ as SaveHabitRequest) >> {
+            SaveHabitRequest request -> throw new PolicyViolationException("habit.constraints.insufficient.notification")
+        }
+
+        when:
+        saveHabitUseCase.execute(new SaveHabitRequest())
+
+        then:
+        def e = thrown(PolicyViolationException)
+        e.message == habit_constraints_insufficient_notification
+    }
+
+    def "habit.achievement.constraints.date.is.in.the.past"() {
+        given:
+        deleteHabitAchievementUseCase.execute(_ as Long, _ as Long) >> {
+            throw new PolicyViolationException("habit.achievement.constraints.date.is.in.the.past")
+        }
+
+        when:
+        deleteHabitAchievementUseCase.execute(0L, 0L)
+
+        then:
+        def e = thrown(PolicyViolationException)
+        e.message == habit_achievement_constraints_date_is_in_the_past
+    }
+
+    def "habit.achievement.constraints.invalid.achievement.date"() {
+        given:
+        deleteHabitAchievementUseCase.execute(_ as Long, _ as Long) >> {
+            throw new PolicyViolationException("habit.achievement.constraints.invalid.achievement.date")
+        }
+
+        when:
+        deleteHabitAchievementUseCase.execute(0L, 0L)
+
+        then:
+        def e = thrown(PolicyViolationException)
+        e.message == habit_achievement_constraints_invalid_achievement_date
+    }
+
+
+    def "social.login.error"() {
+        given:
+        saveMemberUseCase.execute(_ as SaveMemberCommand) >> {
+            throw new ExternalIntegrationException("social.login.error")
+        }
+
+        when:
+        saveMemberUseCase.execute(new SaveMemberCommand())
+
+        then:
+        def e = thrown(ExternalIntegrationException)
+        e.message == social_login_error
+    }
+
+    def "mate.constraints.already.exist.mate"() {
+        given:
+        saveMateUseCase.execute(_ as Long, _ as SaveMateRequest) >> {
+            throw new PolicyViolationException("mate.constraints.already.exist.mate")
+        }
+
+        when:
+        saveMateUseCase.execute(0L, new SaveMateRequest())
+
+        then:
+        def e = thrown(PolicyViolationException)
+        e.message == mate_constraints_already_exist_mate
+    }
+
+    def "mate.constraints.invalid.habit"() {
+        given:
+        saveMateUseCase.execute(_ as Long, _ as SaveMateRequest) >> {
+            throw new PolicyViolationException("mate.constraints.invalid.habit")
+        }
+
+        when:
+        saveMateUseCase.execute(0L, new SaveMateRequest())
+
+        then:
+        def e = thrown(PolicyViolationException)
+        e.message == mate_constraints_invalid_habit
+    }
+
+}
--- app/src/main/java/com/depromeet/threedays/front/web/controller/ApiControllerExceptionHandler.java
+++ app/src/main/java/com/depromeet/threedays/front/web/controller/ApiControllerExceptionHandler.java
@@ -16,13 +16,13 @@ import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
@@ -34,17 +34,17 @@ public class ApiControllerExceptionHandler {
 
 	private static final String FAIL_CODE = "fail";
 
-	private static final String BAD_REQUEST_MESSAGE = "잘못된 요청입니다.";
+	private static final String BAD_REQUEST_MESSAGE = "알 수 없는 오류가 발생했어요.";
 
-	private static final String REFRESH_TOKEN_INVALID_MESSAGE = "로그인이 필요합니다.";
+	private static final String REFRESH_TOKEN_INVALID_MESSAGE = "로그인이 필요해요.";
 
-	private static final String FORBIDDEN_MESSAGE = "접근 권한이 없습니다.";
+	private static final String FORBIDDEN_MESSAGE = "접근 권한이 없어요.";
 
-	private static final String UNAUTHORIZED_MESSAGE = "인증이 필요합니다.";
+	private static final String UNAUTHORIZED_MESSAGE = "인증이 필요해요.";
 
-	private static final String NOT_FOUND_MESSAGE = "요청과 일치하는 결과를 찾을 수 없습니다.";
+	private static final String NOT_FOUND_MESSAGE = "요청과 일치하는 결과를 찾을 수 없어요.";
 
-	private static final String SERVER_ERROR_MESSAGE = "알 수 없는 오류가 발생하였습니다.";
+	private static final String SERVER_ERROR_MESSAGE = "알 수 없는 오류가 발생했어요.";
 
 	@ExceptionHandler(IllegalArgumentException.class)
 	public final ApiResponse<ApiResponse.FailureBody> handleBadRequest(
@@ -54,23 +54,36 @@ public class ApiControllerExceptionHandler {
 	}
 
 	@ExceptionHandler({
-		PolicyViolationException.class,
 		MethodArgumentTypeMismatchException.class,
 		TypeMismatchException.class,
 		ServletRequestBindingException.class,
 		BindException.class,
 		MethodArgumentNotValidException.class,
-		ConstraintViolationException.class,
 		HttpRequestMethodNotSupportedException.class,
 		HttpMediaTypeNotSupportedException.class,
 		HttpMediaTypeNotAcceptableException.class,
 		HttpMessageNotReadableException.class,
-		MissingServletRequestPartException.class
+		MissingServletRequestParameterException.class,
+		ConstraintViolationException.class
 	})
 	public final ApiResponse<ApiResponse.FailureBody> handleBadRequest(
 			final Exception ex, final WebRequest request) {
 		this.writeLog(ex, request);
 		return ApiResponseGenerator.fail(FAIL_CODE, BAD_REQUEST_MESSAGE, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(PolicyViolationException.class)
+	public final ApiResponse<ApiResponse.FailureBody> handleBadRequest(
+			final PolicyViolationException ex, final WebRequest request) {
+		this.writeLog(ex, request);
+		return ApiResponseGenerator.fail(ex.getCode(), ex.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ExternalIntegrationException.class)
+	public final ApiResponse<ApiResponse.FailureBody> handleBadRequest(
+			final ExternalIntegrationException ex, final WebRequest request) {
+		this.writeLog(ex, request);
+		return ApiResponseGenerator.fail(ex.getCode(), ex.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(RefreshTokenInvalidException.class)
@@ -105,7 +118,7 @@ public class ApiControllerExceptionHandler {
 	public ApiResponse<ApiResponse.FailureBody> handleJson(
 			final JsonParsingException ex, final WebRequest request) {
 		this.writeLog(ex, request);
-		return ApiResponseGenerator.fail(FAIL_CODE, FORBIDDEN_MESSAGE, HttpStatus.FORBIDDEN);
+		return ApiResponseGenerator.fail(ex.getCode(), ex.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(Exception.class)
