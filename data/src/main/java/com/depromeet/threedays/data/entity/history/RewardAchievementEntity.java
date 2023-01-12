77bcbbe669e0390f8af82b94232c1187a0151f85
gojung <wjdrndk96@gmail.com>
2023-01-12 17:04:59 +0900

    lint

--- data/src/main/java/com/depromeet/threedays/data/entity/history/RewardAchievementEntity.java
+++ data/src/main/java/com/depromeet/threedays/data/entity/history/RewardAchievementEntity.java
@@ -1,12 +1,11 @@
 package com.depromeet.threedays.data.entity.history;
 
+import java.time.LocalDateTime;
+import javax.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,23 +16,22 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class RewardAchievementEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "reward_achievement_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "reward_achievement_id")
+	private Long id;
 
-    @Column(nullable = false)
-    private Long rewardId;
+	@Column(nullable = false)
+	private Long rewardId;
 
-    @Column(nullable = false)
-    private Long habitAchievementId;
+	@Column(nullable = false)
+	private Long habitAchievementId;
 
-    @Column(nullable = false, updatable = false)
-    @CreatedDate
-    private LocalDateTime createAt;
+	@Column(nullable = false, updatable = false)
+	@CreatedDate
+	private LocalDateTime createAt;
 
-    @Column(nullable = false)
-    @LastModifiedDate
-    private LocalDateTime updateAt;
-
+	@Column(nullable = false)
+	@LastModifiedDate
+	private LocalDateTime updateAt;
 }
