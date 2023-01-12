50d6eb0770d9222b29a0b5802fbb7c9e34a5a9b2
gojung <wjdrndk96@gmail.com>
2023-01-12 13:11:22 +0900

    fix sql error

--- data/migration/V1.00.0.11__add_reward_achievement_relation_table.sql
+++ data/migration/V1.00.0.11__add_reward_achievement_relation_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE `reward_achievement`
     `reward_id`                         varchar(100) DEFAULT NULL,
     `habit_achievement_id`              varchar(255) NOT NULL,
     `create_at`                         datetime(6) NOT NULL,
-    `update_at`                         datetime(6)
+    `update_at`                         datetime(6),
     PRIMARY KEY (`reward_achievement_id`)
 );
