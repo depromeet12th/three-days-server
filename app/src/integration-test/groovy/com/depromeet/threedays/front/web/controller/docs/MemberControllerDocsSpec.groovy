61dab5a4c526d78f532d7e76ef05c6607df4d3ee
saint6839 <saint6839@gmail.com>
2023-01-13 09:47:15 +0900

    DEP-359 fix : MediaType import 추가

--- app/src/integration-test/groovy/com/depromeet/threedays/front/web/controller/docs/MemberControllerDocsSpec.groovy
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/web/controller/docs/MemberControllerDocsSpec.groovy
@@ -22,6 +22,7 @@ import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
