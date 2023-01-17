ef8eb45a1970581671196c30965b6b5dbbd8cc62
saint6839 <saint6839@gmail.com>
2023-01-17 21:21:52 +0900

    DEP-396 feat : 사용자 탈퇴시 fcmToken 값을 비우도록 변경

--- app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/member/DeleteMemberUseCaseSpec.groovy
+++ app/src/integration-test/groovy/com/depromeet/threedays/front/domain/usecase/member/DeleteMemberUseCaseSpec.groovy
@@ -1,34 +1,45 @@
 package com.depromeet.threedays.front.domain.usecase.member
 
+import com.depromeet.threedays.data.entity.client.ClientEntity
+import com.depromeet.threedays.data.entity.member.MemberEntity
 import com.depromeet.threedays.data.enums.MemberStatus
 import com.depromeet.threedays.front.IntegrationTestSpecification
 import com.depromeet.threedays.front.client.AuthClient
 import com.depromeet.threedays.front.client.property.auth.AuthPropertyManager
 import com.depromeet.threedays.front.data.habit.HabitDataInitializer
 import com.depromeet.threedays.front.data.member.MemberInitializer
+import com.depromeet.threedays.front.persistence.repository.client.ClientRepository
 import com.depromeet.threedays.front.persistence.repository.member.MemberRepository
 import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Subject
+
+import java.time.LocalDateTime
 
 class DeleteMemberUseCaseSpec extends IntegrationTestSpecification {
 
+    @Subject
     DeleteMemberUseCase useCase
-    MemberRepository memberRepository
+    MemberRepository repository
     AuthPropertyManager propertyManager
     AuthClient authClient
 
+    @Autowired
+    ClientRepository clientRepository
     @Autowired
     MemberInitializer initializer
     @Autowired
     HabitDataInitializer habitDataInitializer
 
+
     def setup() {
         initializer.initialize()
         habitDataInitializer.initialize()
 
-        memberRepository = Mock(MemberRepository.class)
+        repository = Mock(MemberRepository.class)
         propertyManager = Mock(AuthPropertyManager.class)
         authClient = Mock(AuthClient.class)
-        useCase = new DeleteMemberUseCase(memberRepository, authClient, propertyManager)
+
+        useCase = new DeleteMemberUseCase(repository,  clientRepository, authClient, propertyManager)
     }
 
     def "사용자를 삭제하면 상태가 WITHDRAWN."() {
@@ -40,7 +51,28 @@ class DeleteMemberUseCaseSpec extends IntegrationTestSpecification {
         def actual = useCase.quit(criterionMember.id)
 
         then:
-        memberRepository.findByIdAndStatus(_ as Long, _ as MemberStatus) >> Optional.of(criterionMember)
+        repository.findByIdAndStatus(_ as Long, _ as MemberStatus) >> Optional.of(criterionMember)
         actual.status == MemberStatus.WITHDRAWN
+    }
+
+    def "사용자를 삭제하면 해당 사용자의 fcmToken도 함께 사라진다."() {
+        setup:
+        def criterionMember = initializer.data.first()
+        def criterionClient = ClientEntity
+                .builder()
+                .updateAt(LocalDateTime.now())
+                .createAt(LocalDateTime.now())
+                .memberId(criterionMember.id)
+                .fcmToken("fcmToken")
+                .identificationKey("identificationKey")
+                .build()
+        clientRepository.save(criterionClient)
+
+        when:
+        useCase.deleteFcmToken(criterionMember.id)
+
+        then:
+        def client = clientRepository.findByMemberId(criterionMember.id).get()
+        client.fcmToken == ""
     }
 }
--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/member/DeleteMemberUseCase.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/member/DeleteMemberUseCase.java
@@ -1,5 +1,6 @@
 package com.depromeet.threedays.front.domain.usecase.member;
 
+import com.depromeet.threedays.data.entity.client.ClientEntity;
 import com.depromeet.threedays.data.entity.member.MemberEntity;
 import com.depromeet.threedays.data.enums.CertificationSubject;
 import com.depromeet.threedays.data.enums.MemberStatus;
@@ -11,6 +12,7 @@ import com.depromeet.threedays.front.domain.converter.member.MemberConverter;
 import com.depromeet.threedays.front.domain.model.member.Member;
 import com.depromeet.threedays.front.exception.ExternalIntegrationException;
 import com.depromeet.threedays.front.exception.ResourceNotFoundException;
+import com.depromeet.threedays.front.persistence.repository.client.ClientRepository;
 import com.depromeet.threedays.front.persistence.repository.member.MemberRepository;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -27,38 +29,46 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class DeleteMemberUseCase {
 
-	private final MemberRepository memberRepository;
+	private final MemberRepository repository;
+	private final ClientRepository clientRepository;
 	private final AuthClient authClient;
 	private final AuthPropertyManager propertyManager;
 
 	public Member execute() {
 		Long memberId = AuditorHolder.get();
 		Member member = quit(memberId);
+		deleteFcmToken(memberId);
 		unlinkSocialAccount(memberId);
 		return member;
 	}
 
 	/** 짝심삼일 앱에서 회원 탈퇴 */
 	Member quit(Long memberId) {
-		return memberRepository
+		return repository
 				.findByIdAndStatus(memberId, MemberStatus.REGULAR)
 				.map(MemberEntity::withdraw)
 				.map(MemberConverter::from)
 				.orElseThrow(ResourceNotFoundException::new);
 	}
 
+	void deleteFcmToken(Long memberId) {
+		ClientEntity clientEntity =
+				clientRepository.findByMemberId(memberId).orElseThrow(ResourceNotFoundException::new);
+		clientRepository.save(clientEntity.toBuilder().fcmToken("").build());
+	}
+
 	/** 카카오 계정 연결끊기 */
 	private void unlinkSocialAccount(Long memberId) {
-		MemberEntity member =
-				memberRepository.findById(memberId).orElseThrow(ResourceNotFoundException::new);
-		if (CertificationSubject.KAKAO.equals(member.getCertificationSubject())) {
+		MemberEntity memberEntity =
+				repository.findById(memberId).orElseThrow(ResourceNotFoundException::new);
+		if (CertificationSubject.KAKAO.equals(memberEntity.getCertificationSubject())) {
 			try {
 				AuthRequestProperty property =
-						propertyManager.getMemberProperty(member.getCertificationSubject());
+						propertyManager.getMemberProperty(memberEntity.getCertificationSubject());
 
 				Map<String, Object> form = new HashMap<>();
 				form.put("target_id_type", "user_id");
-				form.put("target_id", Long.parseLong(member.getCertificationId()));
+				form.put("target_id", Long.parseLong(memberEntity.getCertificationId()));
 				String adminKey = "KakaoAK " + property.getAdminKey();
 				authClient.unlink(new URI(property.getHost() + property.getUnlink()), adminKey, form);
 			} catch (URISyntaxException e) {
@@ -70,7 +80,7 @@ public class DeleteMemberUseCase {
 	public Member executeCallback(CertificationSubject subject, String key, String userId) {
 		AuthRequestProperty property = propertyManager.getMemberProperty(subject);
 		if (key != null && key.equals(property.getAdminKey())) {
-			return memberRepository
+			return repository
 					.findByCertificationIdAndCertificationSubjectAndStatus(
 							userId, subject, MemberStatus.REGULAR)
 					.map(MemberEntity::withdraw)
