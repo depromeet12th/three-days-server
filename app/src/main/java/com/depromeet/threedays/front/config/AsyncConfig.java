77c59c31c78f7eac35ad07fba664be659beaf48e
saint6839 <saint6839@gmail.com>
2023-01-20 22:01:03 +0900

    DEP-396 feat : 비동기 예외 핸들러 추가, 스레드풀 크기 초과 예외 대응 로직 추가

--- app/src/main/java/com/depromeet/threedays/front/config/AsyncConfig.java
+++ app/src/main/java/com/depromeet/threedays/front/config/AsyncConfig.java
@@ -1,21 +1,33 @@
 package com.depromeet.threedays.front.config;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 	@Bean(name = "threadPoolTaskExecutor")
 	public Executor threadPoolTaskExecutor() {
 		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-		taskExecutor.setCorePoolSize(3); // 기본 스레드 수
-		taskExecutor.setMaxPoolSize(30); // 최대 스레드 수
-		taskExecutor.setQueueCapacity(100); // Queue 사이즈
+		taskExecutor.setCorePoolSize(5);
+		taskExecutor.setMaxPoolSize(50);
+		taskExecutor.setQueueCapacity(100);
 		taskExecutor.setThreadNamePrefix("Executor-");
+		taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		taskExecutor.setAwaitTerminationSeconds(20);
+		taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
 		return taskExecutor;
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return new SimpleAsyncUncaughtExceptionHandler();
 	}
 }
