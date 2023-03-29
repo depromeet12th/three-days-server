88fcb6b9b35bce44ac90f1ab6c200aa3becb918d
belljun3395 <195850@jnu.ac.kr>
2023-03-30 01:08:53 +0900

    DEP-405 refactor : SignMemberUseCaseFacade validateToken 분리
    + validateToken : TokenAuthenticator.authenticateIdToken 통해 분리

--- app/src/main/java/com/depromeet/threedays/front/domain/usecase/member/SignMemberUseCaseFacade.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/usecase/member/SignMemberUseCaseFacade.java
@@ -7,14 +7,13 @@ import com.depromeet.threedays.front.client.model.KeyProperties;
 import com.depromeet.threedays.front.client.model.MemberInfo;
 import com.depromeet.threedays.front.client.property.auth.AppleAuthProperty;
 import com.depromeet.threedays.front.client.property.auth.AuthRequestProperty;
-import com.depromeet.threedays.front.config.security.filter.token.IdTokenProperties;
 import com.depromeet.threedays.front.config.security.filter.token.TokenGenerator;
 import com.depromeet.threedays.front.config.security.filter.token.TokenResolver;
 import com.depromeet.threedays.front.domain.converter.member.AppleAuthRequestWithCodeConverter;
 import com.depromeet.threedays.front.domain.converter.member.MemberCommandConverter;
 import com.depromeet.threedays.front.domain.converter.member.MemberQueryConverter;
 import com.depromeet.threedays.front.domain.model.member.SaveMemberUseCaseResponse;
-import com.depromeet.threedays.front.domain.validation.TokenValidator;
+import com.depromeet.threedays.front.domain.validation.TokenAuthenticator;
 import com.depromeet.threedays.front.exception.ExternalIntegrationException;
 import com.depromeet.threedays.front.support.RequestBodyGenerator;
 import com.depromeet.threedays.front.web.request.member.AppleSignMemberRequest;
@@ -36,7 +35,7 @@ public class SignMemberUseCaseFacade {
 	private final GetMemberUseCase getUseCase;
 	private final SaveMemberUseCase saveUseCase;
 	private final AuthClient authClient;
-	private final TokenValidator tokenValidator;
+	private final TokenAuthenticator tokenAuthenticator;
 	private final TokenGenerator tokenGenerator;
 	private final TokenResolver tokenResolver;
 
@@ -98,7 +97,8 @@ public class SignMemberUseCaseFacade {
 		// todo check 위에서는 NoSuchFieldError::new를 발생시키에 현 상황에서 가장 적당한 IllegalAccessError::new를 선택
 		AppleAuthProperty property =
 				getAppleProperty(request.getCertificationSubject()).orElseThrow(IllegalAccessError::new);
-		validateToken(property, request);
+		KeyProperties keyProperties = getKeyProperties(property);
+		tokenAuthenticator.authenticateIdToken(property, keyProperties, request);
 		AppleTokenInfo tokenInfo = getToken(property, request.getCode());
 		String certificationId = tokenResolver.extractSubByToken(tokenInfo.getIdToken());
 		return MemberInfo.builder().id(certificationId).name(request.getName()).build();
@@ -108,14 +108,6 @@ public class SignMemberUseCaseFacade {
 		return subject == CertificationSubject.APPLE
 				? Optional.of((AppleAuthProperty) getMemberProperty(subject))
 				: Optional.empty();
-	}
-
-	private void validateToken(AppleAuthProperty property, AppleSignMemberRequest request) {
-		KeyProperties keyProperties = getKeyProperties(property);
-		tokenValidator.validateIdTokenByKeys(keyProperties, request.getIdToken());
-		IdTokenProperties idTokenProperties =
-				tokenResolver.extractPropertiesByToken(request.getIdToken());
-		tokenValidator.validateIdToken(property, request, idTokenProperties);
 	}
 
 	private KeyProperties getKeyProperties(AppleAuthProperty property) {
