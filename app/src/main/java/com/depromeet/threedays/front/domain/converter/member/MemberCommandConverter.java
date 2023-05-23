7dfc7e54c41ff7194feba5af083c20b73482211c
belljun3395 <195850@jnu.ac.kr>
2023-05-23 22:44:56 +0900

    DEP-405 feat : Apple 로그인 탈퇴를 위한 refreshToken 저장을 위한 from 메서드 추가

--- app/src/main/java/com/depromeet/threedays/front/domain/converter/member/MemberCommandConverter.java
+++ app/src/main/java/com/depromeet/threedays/front/domain/converter/member/MemberCommandConverter.java
@@ -1,5 +1,6 @@
 package com.depromeet.threedays.front.domain.converter.member;
 
+import com.depromeet.threedays.front.client.model.AppleMemberInfo;
 import com.depromeet.threedays.front.client.model.MemberInfo;
 import com.depromeet.threedays.front.domain.command.SaveMemberCommand;
 import com.depromeet.threedays.front.web.request.member.SignMemberRequest;
@@ -22,5 +23,27 @@ public class MemberCommandConverter {
 				.certificationId(data.getId())
 				.notificationConsent(true)
 				.build();
+	}
+
+	public static SaveMemberCommand from(
+			final AppleMemberInfo data, final SignMemberRequest request) {
+		if (data == null || request == null) {
+			return null;
+		}
+
+		return SaveMemberCommand.builder()
+				.resource(getResource(data).toString())
+				.name(data.getName(request.getCertificationSubject()))
+				.certificationSubject(request.getCertificationSubject())
+				.memberInfo(data)
+				.certificationId(data.getId())
+				.notificationConsent(true)
+				.build();
+	}
+
+	private static JsonObject getResource(AppleMemberInfo data) {
+		JsonObject resource = new JsonObject();
+		resource.addProperty("refreshToken", data.getRefreshToken());
+		return resource;
 	}
 }
