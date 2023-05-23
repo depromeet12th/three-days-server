9c5dfa7ac7c205b810bdd051e7b27662235e26f8
belljun3395 <195850@jnu.ac.kr>
2023-05-23 22:49:45 +0900

    DEP-405 feat : RequestBodyGenerator generateAppleAuthRevokeRequestBody 구현

--- app/src/main/java/com/depromeet/threedays/front/support/RequestBodyGenerator.java
+++ app/src/main/java/com/depromeet/threedays/front/support/RequestBodyGenerator.java
@@ -2,6 +2,7 @@ package com.depromeet.threedays.front.support;
 
 import com.depromeet.threedays.front.domain.query.member.AppleAuthRequestWithCodeQuery;
 import com.depromeet.threedays.front.domain.query.member.AppleAuthRequestWithTokenQuery;
+import com.depromeet.threedays.front.domain.query.member.AppleAuthRevokeRequestQuery;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.experimental.UtilityClass;
@@ -14,6 +15,8 @@ public class RequestBodyGenerator {
 	private static final String CODE_KEY = "code";
 	private static final String REFRESH_TOKEN_KEY = "refresh_token";
 	private static final String GRANT_TYPE_KEY = "grant_type";
+	private static final String TOKEN_KEY = "token";
+	private static final String TOKEN_TYPE_HINT = "token_type_hint";
 	private static final String AUTHORIZATION_CODE_VALUE = "authorization_code";
 	private static final String REFRESH_TOKEN_VALUE = "refresh_token";
 
@@ -34,6 +37,16 @@ public class RequestBodyGenerator {
 		body.put(CLIENT_SECRET_KEY, query.getClientSecret());
 		body.put(GRANT_TYPE_KEY, REFRESH_TOKEN_VALUE);
 		body.put(REFRESH_TOKEN_KEY, query.getRefreshToken());
+		return body;
+	}
+
+	public static Map<String, String> generateAppleAuthRevokeRequestBody(
+			AppleAuthRevokeRequestQuery query) {
+		Map<String, String> body = new HashMap<>();
+		body.put(CLIENT_ID_KEY, query.getClientId());
+		body.put(CLIENT_SECRET_KEY, query.getClientSecret());
+		body.put(TOKEN_KEY, query.getToken());
+		body.put(TOKEN_TYPE_HINT, REFRESH_TOKEN_VALUE);
 		return body;
 	}
 }
