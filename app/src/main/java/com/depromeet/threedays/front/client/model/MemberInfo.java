d6bbe8f2eae55e1c11e1dfeaa084cf39f4e74d4e
belljun3395 <195850@jnu.ac.kr>
2023-05-23 22:42:58 +0900

    DEP-405 feat : AppleMemberInfo를 위한 MemberInfo 생성자 추가

--- app/src/main/java/com/depromeet/threedays/front/client/model/MemberInfo.java
+++ app/src/main/java/com/depromeet/threedays/front/client/model/MemberInfo.java
@@ -2,18 +2,23 @@ package com.depromeet.threedays.front.client.model;
 
 import com.depromeet.threedays.data.enums.CertificationSubject;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Getter
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberInfo {
 	private String id;
 	private String name;
 	private Properties properties;
+
+	public MemberInfo(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 	@Getter
 	@AllArgsConstructor
