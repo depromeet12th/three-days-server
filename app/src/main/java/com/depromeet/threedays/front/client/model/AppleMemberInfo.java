a0ce1ee4ff3aad768ec732b0971d1883853d4871
belljun3395 <195850@jnu.ac.kr>
2023-05-23 22:42:45 +0900

    DEP-405 feat : AppleMemberInfo 추가

---
+++ app/src/main/java/com/depromeet/threedays/front/client/model/AppleMemberInfo.java
@@ -0,0 +1,19 @@
+package com.depromeet.threedays.front.client.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppleMemberInfo extends MemberInfo {
+
+	private String refreshToken;
+
+	public AppleMemberInfo(String id, String name) {
+		super(id, name);
+	}
+}
