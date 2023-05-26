2ab894a55eff3e5f87db30dec61b52bc87394564
belljun3395 <195850@jnu.ac.kr>
2023-05-26 19:35:26 +0900

    DEP-405 refactor : 테스트 코드 작성을 위한 AppleSignMemberRequest 생성자 추가 및 getter에 @JsonIgnore 추가

--- app/src/main/java/com/depromeet/threedays/front/web/request/member/AppleSignMemberRequest.java
+++ app/src/main/java/com/depromeet/threedays/front/web/request/member/AppleSignMemberRequest.java
@@ -1,5 +1,7 @@
 package com.depromeet.threedays.front.web.request.member;
 
+import com.depromeet.threedays.data.enums.CertificationSubject;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,27 @@ public class AppleSignMemberRequest extends SignMemberRequest {
 	private String nonce;
 	private AppleUserInfo user;
 
+	private AppleSignMemberRequest(
+			CertificationSubject certificationSubject,
+			String socialToken,
+			String code,
+			String nonce,
+			String email,
+			String firstName,
+			String lastName) {
+		super(certificationSubject, socialToken);
+		this.code = code;
+		this.nonce = nonce;
+		this.user = new AppleUserInfo(email, firstName, lastName);
+	}
+
+	@JsonIgnore
 	public String getName() {
 		return this.getUser().getName();
 	}
 
 	/** socialToken과 idToken은 동일하다. */
+	@JsonIgnore
 	public String getIdToken() {
 		return super.getSocialToken();
 	}
